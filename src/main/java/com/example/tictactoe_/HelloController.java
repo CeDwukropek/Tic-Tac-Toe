0018d80b17a3ddc37771cb1367febf430c400a28
Emil Kopytek <emilkopytek@gmail.com>
2023-11-17 11:43:38 +0100

    Working layhout

--- src/main/java/com/example/tictactoe_/HelloController.java
+++ src/main/java/com/example/tictactoe_/HelloController.java
@@ -1,11 +1,18 @@
 package com.example.tictactoe_;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button buttonTest;
+
+    HelloController() {
+        buttonTest.setPercentWidth(100.0/horizontalGridCount);    
+    }
 
     @FXML
     protected void onHelloButtonClick() {
