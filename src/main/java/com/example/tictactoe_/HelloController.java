2e5898277da7951b82aeac12339a7b29173a4193
Emil Kopytek <emilkopytek@gmail.com>
2023-11-20 14:40:21 +0100

    basic working layout

--- src/main/java/com/example/tictactoe_/HelloController.java
+++ src/main/java/com/example/tictactoe_/HelloController.java
@@ -1,21 +1,49 @@
 package com.example.tictactoe_;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.EventListener;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
     @FXML
     private Button buttonTest;
+    protected boolean turn = true;
+    protected int[][] tab;
 
-    HelloController() {
-        buttonTest.setPercentWidth(100.0/horizontalGridCount);    
+    public HelloController() {
+        this.tab = new int[][]{
+                {-1, -1, -1},
+                {-1, -1, -1},
+                {-1, -1, -1}};
     }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void buttonEvent(Event e) {
+        if(this.turn) {
+            move((Button)e.getSource(), "O");
+        } else {
+            move((Button)e.getSource(), "X");
+        }
+
+        System.out.println(
+                ((Control)e.getSource()).getId()
+        );
+    }
+
+    public void move(Button btn, String v) {
+        if(btn.getText().equals("X") || btn.getText().equals("O")) return;
+        if(btn.getText().isEmpty()) {
+            btn.setText(v);
+            this.turn = !this.turn;
+            // btn.setUserData() i btn.getUserData() !
+        }
     }
 }
